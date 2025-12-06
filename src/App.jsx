133654201840106429c2df129be26ac22f70c3ad
DarkDev <work639280@gmail.com>
2025-12-06 22:21:58 +0530

    initial commit of the App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Profile from "./components/Profile";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
-import Requests from "./components/requests";
-import Connections from "./components/connections";
+import Requests from "./components/Requests";
+import Connections from "./components/Connections";
 
 function App() {
   return (
